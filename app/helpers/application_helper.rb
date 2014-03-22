a8ddf993bb394b614511970b676b60aba5d70975
mrchrisfrost <androidpatmdev@gmail.com>
2014-03-22 02:14:49 -0400

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
 
   # Returns the full title on a per-page basis.
-  def full_title(page_title)
-     base_title = "My Sample Application Under Rails"  
-     if page_title.empty? 
-	base_title
+  def full_title(page_title)          #Method definition.
+     base_title = "My Sample Application Under Rails"  #Variable assignment
+     if page_title.empty?   #boolean test
+	base_title          #Implicit return 
      else 
-	"#{base_title} | #{page_title}"
+	"#{base_title} | #{page_title}"  #String interpolation 
      end 
    end 
 end
